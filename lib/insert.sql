ca904ff993a306090c3f24e9ffb8155349808cfc
fahmy-dev <faahmy@proton.me>
2025-03-01 16:12:53 +0300

    initial commit

---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Mr. Chocolate', 4, 'M', 'Black', 'Calm', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Rowdy', 8, 'F', 'Purple', 'Calm', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Tabitha', 3, 'F', 'Brown', 'Playful', 0);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Sergeant Brown', 3, 'M', 'Blue', 'Quiet', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Melissa', 9, 'M', 'Green', 'Loud', 0);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Grinch', 2, 'F', 'White', 'Scary', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Wendy', 5, 'F', 'Red', 'Calm', 0);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES (NULL, 6, 'M', 'Brown', 'Playful', 1);
